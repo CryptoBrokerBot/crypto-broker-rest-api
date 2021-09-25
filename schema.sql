793bb25684c93d5c8c5d66ff326ccecbb60596ee
Jonathan Cooper <drowsysaturn@gmail.com>
2021-09-24 20:48:18 -0400

    Switch to coingecko token id

--- schema.sql
+++ schema.sql
@@ -15,11 +15,11 @@ CREATE INDEX IDX_cryptoname ON cryptodata(name); -- name lookups are now fast :)
 
 
 -- Wallet for each user for each server, each user can participate in multiple servers.
-CREATE TABLE serverWallets (
-  walletId SERIAL PRIMARY KEY,
-  userId VARCHAR(256),
+CREATE TABLE wallet (
+  userId VARCHAR(256) PRIMARY KEY,
   walletBalance NUMERIC(25,4)
 );
+
 
 CREATE TABLE serverpatrons (
   serverId VARCHAR(256),
@@ -28,18 +28,17 @@ CREATE TABLE serverpatrons (
 );
 CREATE INDEX IDX_serverpatrons_serverId on serverpatrons(serverId);
 
-
 -- transactions belong to a serverwallet (which is unique to a (userId,serverId) combo)
 CREATE TABLE transactions (
   transactionId SERIAL,
-  transactionTime TIMESTAMP NOT NULL,
-  walletId INT,--Looks like SERIAL is INT in pg VARCHAR(256),
+  transactionTime TIMESTAMP NOT NULL DEFAULT NOW(),
+  userId VARCHAR(256),--Looks like SERIAL is INT in pg VARCHAR(256),
   cryptoId VARCHAR(256),
   cost NUMERIC(25,4) NOT NULL, -- negative indicates a sell positive indicates a buy
   buySellIndicator CHAR(1) NOT NULL, -- makes life a little bit easier so we dont have to compare cost to 0 to get Buy or Sell
   qty NUMERIC(25,8) NOT NULL,-- amount of crypto purchased / sold positive indicates buy and negative indicates sell
-  PRIMARY KEY (transactionId,walletId),
-  CONSTRAINT CHK_buySellIndicator CHECK(buySellIndicator in ('B','S')) 
+  PRIMARY KEY (transactionId),
+  CONSTRAINT CHK_buySellIndicator CHECK(buySellIndicator in ('B','S'))
   --This constraint syntax might be for another db software, TODO: fix this
   --CONSTRAINT PK_transaction PRIMARY KEY (transactionId,userId),
   --CONSTRAINT FK_transactionSymbol FOREIGN KEY (symbol) REFERENCES cryptodata(symbol) ON DELETE CASCADE,
@@ -49,17 +48,29 @@ CREATE TABLE transactions (
 
 -- portfolio, and leaderboards should be views since they inherently change all the time and can be calculated from the data above
 
-
 CREATE VIEW vPortfolio AS
-SELECT w.userId, 
-t.cryptoId, 
-SUM(t.cost) as totalCost,
-SUM(t.qty) as qty, 
-MAX(t.transactionTime) as lastTransactionTs
-FROM transactions t JOIN serverWallets w on t.walletId = w.walletId
-GROUP BY 
-w.userId,
-t.cryptoId;
+WITH cteLatestPrices AS (
+  SELECT id as cryptoId, price as latestPrice FROM (SELECT id, price, ROW_NUMBER() OVER(partition by id order by asOf DESC) as rn
+  FROM cryptodata) res WHERE res.rn = 1
+),
+ctePositions AS (
+  SELECT w.userId, 
+  t.cryptoId, 
+  SUM(t.qty) as qty,
+  MAX(t.transactionTime) as lastTransactionTs
+  FROM transactions t 
+  JOIN wallet w on t.userId = w.userId
+  GROUP BY 
+    w.userId,
+    t.cryptoId
+)
+SELECT userId,
+p.cryptoId, 
+qty*latestPrice as currentValue,
+lastTransactionTs
+FROM ctePositions p
+JOIN cteLatestPrices lp on p.cryptoId = lp.cryptoId
+
 
 
 -- CREATE VIEW vWalletPerformance AS
